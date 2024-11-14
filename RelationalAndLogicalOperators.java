a4364d908079419262f732625982a6c43c2b0a05
Tirumalesh J <92926731+tirunutsy@users.noreply.github.com>
2024-11-14 22:43:56 +0530

    Add files via upload
    Here We used all the Relational and Logical Operators to get better idea about how they behave

---
+++ RelationalAndLogicalOperators.java
@@ -0,0 +1,28 @@
+package AllPrograms;
+
+public class RelationalAndLogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5, b=6;
+		boolean x=true, y=false;
+		
+//		Relational Operators
+		System.out.println("Relational Operators :");
+		System.out.println("a == b : "+(a==b));
+		System.out.println("a != b : "+(a!=b));
+		System.out.println("a > b : "+(a>b));
+		System.out.println("a < b : "+(a<b));
+		System.out.println("a >= b : "+(a>=b));
+		System.out.println("a <= b : "+(a<=b));
+		
+//		Logical Operators
+		System.out.println("Logical Operators :");
+		System.out.println("x && y : "+(x && y));
+		System.out.println("x || y : "+(x || y));
+		System.out.println("!x : "+(!x));
+		
+	}
+
+}
