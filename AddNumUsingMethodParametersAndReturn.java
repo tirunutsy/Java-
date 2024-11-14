c9590b570c2c1db1e9212df2ac0a59747ea42c79
Tirumalesh J <92926731+tirunutsy@users.noreply.github.com>
2024-11-14 22:35:19 +0530

    Add files via upload
    Here in this program we have done adding Two Numbers Using Method Parameters and Return Type

---
+++ AddNumUsingMethodParametersAndReturn.java
@@ -0,0 +1,23 @@
+package AllPrograms;
+
+public class AddNumUsingMethodParametersAndReturn {
+	
+	static void AddMethodParameters(int a, int b) {
+		int temp = a+b;
+		System.out.println("Method parameters A+B :"+temp);
+	}
+	
+	static int AddReturnType(int a, int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AddMethodParameters(10,11);
+//		AddReturnType(6,5);
+		System.out.println("Return Type A+B :"+AddReturnType(6,5));
+		
+	}
+
+}
